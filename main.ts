50dc763ad338ba625bf1043135b8f3ca0fef3015
StarNumber12046 <64470722+StarNumber12046@users.noreply.github.com>
2022-10-07 11:48:06 +0200

    feat(base): now connects to MongoDB

--- main.ts
+++ main.ts
@@ -2,16 +2,16 @@ import express from "express" ;
 const port = 3000
 const app = express()
 import * as mongoose from "mongoose";
+import * as dotenv from "dotenv"
 import * as bp  from 'body-parser';
 app.use(bp.json())
 app.use(bp.urlencoded({ extended: true }))
 
-mongoose.connect('mongodb://admin:AMOGUS@192.168.0.200:27017/ReFasm?authSource=admin&readPreference=primary&ssl=false');
+dotenv.config(".env")
+
+const connection = mongoose.connect(process.env.MONGODB_CONNECTION_STRING);
 const ShortenedUrlModel = new mongoose.Schema({ slug: String, url: String })
 const ShortenedUrl = mongoose.model('Url', ShortenedUrlModel);
-
-const shit = new ShortenedUrl({ slug: 'shit', url: 'https://example.com' });
-shit.save().then((value) => console.log('shit be like: ', value));
 
 
 const keys = {"foo": "bar"}
@@ -20,13 +20,16 @@ app.get("/", function(req, res) {
     res.send({"status": "OK", "response": "Hi from ReFasm.ga api"});
 })
 
-app.post("/create", (req, res) => {
+app.post("/create", async (req, res) => {
+    
     let headers = req.headers
     let body = req.body
     const token = headers.authorization
+    
     if (!body.hasOwnProperty("url")) {
         return res.send({"status": "ERROR", "response": "No url provided"})
     }
+    
     if (!body.hasOwnProperty("slug")) {
         var slug = (Math.random() + 1).toString(36).substring(6);
     }
@@ -41,8 +44,18 @@ app.post("/create", (req, res) => {
     if (!keys[token]) {
         return res.send({"status": "ERROR", "response": "Invalid Authorization Token"})
     }
-    return res.send({"status": "OK", "response": "shortened", "link": "refasm.ga/", slug})
-})
+    const return_value = await ShortenedUrl.find({ slug: slug })
+    if (return_value.length == 0) {
+    const shortened = new ShortenedUrl({ slug:slug, url:url })
+    shortened.save()
+      .then((value) => {
+        console.log("Saved!")
+    })
+    return res.send({"status": "OK", "response": "shortened", "link": "refasm.ga/"+slug})}
+    else {
+        return res.send({"status": "ERROR", "response": "slug already in use"})}
+    }
+)
 
 app.listen(port, () => {
     console.log(`ReFasm API listening on ${port}`)
